c5befe5b56661731926dd0f5b223fc994ab7ae59
Lii02 <lukeinlow@gmail.com>
2023-04-20 18:08:15 -0400

    Big bang

---
+++ lookup.py
@@ -0,0 +1,10 @@
+import common
+
+def main():
+    input_link = input("Insert the ID to lookup: ")
+    if not input_link:
+        print("No ID was entered")
+        return
+
+if __name__ == "__main__":
+    main()
---
+++ shorten.py
@@ -0,0 +1,10 @@
+import common
+
+def main():
+    input_link = input("Insert the link to shorten: ")
+    if not input_link:
+        print("No link was entered")
+        return
+
+if __name__ == "__main__":
+    main()
